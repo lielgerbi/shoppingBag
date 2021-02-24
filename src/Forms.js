178df61369053967ca7b7a2e6e2b5af85dbf53a4
lielgerbi <78143900+lielgerbi@users.noreply.github.com>
2021-02-24 13:45:20 +0200

    creat component

---
+++ src/Forms.js
@@ -0,0 +1,146 @@
+import React ,  {useState} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useForm } from "react-hook-form";
+import swal from 'sweetalert';
+import "./app.css"
+import Axios from "axios";
+function Forms({rows}) {
+    
+    const [forms, setForm] = useState(true);
+    const { register, handleSubmit } = useForm();
+    const [selectCategory, setCategory] = useState(null);
+    const categories = ["meat",
+      "milk",
+      "snacks"]
+  
+    const products = [{ name: "שניצל", category: categories[0] },
+    { name: "חלב", category: categories[1] },
+    { name: "שוקולד", category: categories[2] },
+    { name: "מילקי", category: categories[1] },
+    { name: "שוקו", category: categories[1] },
+    { name: "גלידה", category: categories[1] },
+    { name: "יוגורט", category: categories[1] },
+    { name: "דוריטוס", category: categories[2] },
+    { name: "במבה", category: categories[2] },
+    { name: "ציפס", category: categories[2] },
+    { name: "ביסלי", category: categories[2] },
+    { name: "פרינגלס", category: categories[2] },
+    { name: "עוף", category: categories[0] },
+    { name: "דג", category: categories[0] },
+    { name: "שווארמה", category: categories[0] },
+    { name: "צלי", category: categories[0] },
+    { name: "סטייק", category: categories[0] }]
+    function getItemsByCat(category) {
+        var filterItems = products.filter(product => product.category === category);
+        return filterItems;
+      }
+      function addProdoct(newItem) {
+        Axios({
+          method: "get",
+          url: "http://localhost:5000/additem",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          params: {
+            items: newItem
+          }
+        }).then(res => {
+          console.log(res)
+        });
+        // setRows(rows);
+        // setCount(rows.length);
+      }
+    
+      const onSubmitForm = data => {
+        if (forms === true) {
+          isInList({ id: (rows.length + 1).toString(), name: data.name, category: data.category, price: data.price, quantity: data.quantity })
+        }
+        else {
+          isInList({ id: (rows.length + 1).toString(), name: data.name1, category: data.category1, price: data.price1, quantity: data.quantity1 })
+        }
+      };
+
+      function reloadRows() {
+        Axios({
+          method: "GET",
+          url: "http://localhost:5000/",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }).then(res => {
+          // sortArr(selectCategory);
+          window.location.reload(false);
+        });
+      }
+      function isInList(newItem) {
+        Axios({
+          method: "get",
+          url: "http://localhost:5000/getItemIndex",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          params: {
+            items: newItem
+          }
+        }).then(res => {
+          if (res.data.index === -1) {
+            addProdoct(newItem)
+            reloadRows();
+          }
+          else {
+            swal("product already in list", "", "error");
+          }
+        });
+    
+      }
+
+   React.useEffect(() => {
+   });
+     return (
+        <div>
+            <div id="divInleft">
+            <form onSubmit={handleSubmit(onSubmitForm)}>
+              <label for="colFormLabel" class="col-sm-2 col-form-label">category</label>
+              <select required name="category1" id="inputState" ref={register} onChange={e => setCategory(e.currentTarget.value)} class="form-control">
+                <option>category </option>
+                {categories.map(item => {
+                  return (
+                    <option value={item}> {item} </option>
+                  )
+                })}
+              </select>
+              <label for="colFormLabel" class="col-sm-2 col-form-label" required>name</label>
+              <select required name="name1" id="inputState" ref={register} class="form-control">
+                {getItemsByCat(selectCategory).map(item => {
+                  return (
+                    <option value={item.name}> {item.name} </option>
+                  )
+                })}
+              </select>
+
+              <label for="colFormLabel" class="col-sm-2 col-form-label">quantity</label>
+              <input required name="quantity1" type="text" ref={register} class="form-control form-control-lg" id="colFormLabelLg" placeholder="quantity" />
+              <label for="colFormLabel" class="col-sm-2 col-form-label">price</label>
+              <input required name="price1" type="text" ref={register} pattern="[1-9]" title="not good" class="form-control form-control-lg" id="colFormLabelLg" placeholder="price" />
+              <button type="submit" onClick={() => setForm(false)} class="btn btn-primary" >add item</button>
+            </form>
+          </div>
+
+          <div id="divInright">
+
+            <form onSubmit={handleSubmit(onSubmitForm)}>
+              <label for="colFormLabel" class="col-sm-2 col-form-label">name</label>
+              <input required name="name" type="text" ref={register} class="form-control form-control-lg" id="colFormLabelLg" placeholder="name" />
+              <label for="colFormLabel" class="col-sm-2 col-form-label">category</label>
+              <input required name="category" type="text" ref={register} class="form-control form-control-lg" id="colFormLabelLg" placeholder="category" />
+              <label for="colFormLabel" class="col-sm-2 col-form-label">quantity</label>
+              <input required name="quantity" type="text" ref={register} class="form-control form-control-lg" id="colFormLabelLg" placeholder="quantity" />
+              <label for="colFormLabel" class="col-sm-2 col-form-label">price</label>
+              <input required name="price" type="text" ref={register} class="form-control form-control-lg" id="colFormLabelLg" placeholder="price" />
+              <button type="submit" onClick={() => setForm(true)} class="btn btn-primary" >add item</button>
+            </form>
+          </div>
+        </div>
+      );
+  }
+  export default Forms;
---
+++ src/ItemTable.js
@@ -0,0 +1,75 @@
+import React ,  {} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./app.css"
+import Axios from "axios";
+function ItemTable({rows}) {
+
+    function getsum() {
+        return rows.reduce((a, b) => a + ((b.price || 0) * (b.quantity || 0)), 0);
+      }
+      function deleteItem(newItem) {
+        Axios({
+          method: "get",
+          url: "http://localhost:5000/removeitem",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          params: {
+            items: newItem
+          }
+        }).then(res => {
+        //   setCount(res.data.rows.length);
+         // setRows(res.data.rows);
+         window.location.reload(false);
+        });
+      }
+   React.useEffect(() => {
+   });
+     return (
+        <div>
+          <div id="divInCenter">
+              <div id="table-wrapper">
+                <table id="process-manager-table" class="table table-sm">
+                  <tbody>
+                    <tr>
+                      <th scope="col">
+                        <button type="button" class="btn btn-outline-secondary">name ⇅</button></th>
+                      <th scope="col">
+                        <button type="button"  class="btn btn-outline-secondary">category ⇅ </button></th>
+                      <th scope="col">
+                        <button type="button"  class="btn btn-outline-secondary">price ⇅ </button></th>
+                      <th scope="col">
+                        <button type="button" class="btn btn-outline-secondary">quantity ⇅</button></th>
+                      <th scope="col">
+                        <button type="button" class="btn btn-outline-secondary">total price ⇅</button></th>
+                    </tr>
+                    {rows.map(item => {
+                      return (
+                        <tr>
+                          <td>{item.name}</td>
+                          <td>{item.category}</td>
+                          <td>{item.price}</td>
+                          <td>{item.quantity}</td>
+                          <td>{item.quantity * item.price}</td>
+                          <button className={`pointer`} onClick={() => deleteItem(item)}>🗑️</button>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+              <div id="totalPrice">
+                מחיר סופי:{getsum()}
+                {getsum() < 20 &&
+                  <div>✔</div>
+
+                }
+                {getsum() > 20 &&
+                  <div>❌</div>
+                }
+              </div>
+            </div>
+        </div>
+      );
+  }
+  export default ItemTable;
